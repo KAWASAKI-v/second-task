d247032efb4e859adb513a88f92e06b4896daf60
KAWASAKI-v <qwinki192@gmail.com>
2024-12-19 14:12:31 +0300

    Add files via upload

---
+++ Zadanie2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "sum.h"
+
+using namespace std;
+
+int main()
+
+{
+	int z;	
+	int x;	
+	cout<< "2 num separate by enter"<<endl;
+	cin >>z;
+	cin >>x;
+	cout<<"Sum = "<<sum(z,x)<<endl;
+	return 0;
+	
+}
